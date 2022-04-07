a3d694aad08e0d6b9d0b1289cc739a45434b1da4
samwel-chege <samwelchegeh09@gmail.com>
2022-04-07 10:31:41 +0300

    install react icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react"
 
-function App() {
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Follow up meeting',
+        day: 'April 8th at 11:00am ',
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: 'Meeting at Ada labs',
+        day: 'April 10th at 2:00pm ',
+        reminder: true,
+    },
+
+    {
+        id: 3,
+        text: 'Finish up oracle',
+        day: 'April 9th at 11:00am ',
+        reminder: false,
+    },
+
+    {
+        id: 4,
+        text: 'Pick up stuff',
+        day: 'April 12th at 11:00am ',
+        reminder: true,
+    },
+    
+
+])
+
   return (
     <div className="container">
     <Header/>
+    <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = (
   { title }
 ) => {
-  return (
-    <header>
-        <h1>{title}</h1>
-        <button></button>
 
+  const onClick = () => {
+    console.log('Click')
+  }
+  return (
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text='Add' onClick={onClick} />
+        
     </header>
   )
 }
