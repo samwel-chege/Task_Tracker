1f9185e20367e4ebac6479fc6c69a29a02feb90b
samwel-chege <samwelchegeh09@gmail.com>
2022-04-07 12:48:35 +0300

    create a addTask component

--- src/App.js
+++ src/App.js
@@ -35,11 +35,21 @@ const App = () => {
     
 
 ])
+// delete tasks
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+//Toggle reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
 
   return (
     <div className="container">
     <Header/>
-    <Tasks tasks={tasks} />
+    {tasks.length > 0 ? <Tasks tasks={tasks} onDelete = {deleteTask} onToggle={toggleReminder} /> : 'Hello friend, we do not have tasks.'}
     </div>
   );
 }
